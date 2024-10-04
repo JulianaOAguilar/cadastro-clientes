577f5bfc3d0693f0792333ab45f57a625a66de8f
Xinlana1 <julianaoaguilar16@gmail.com>
2024-10-04 14:35:31 -0300

    Close #3 - Api e configurar info do CEP

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,25 @@
 $('#Cep').mask('00000-000');
+
+
+
+
+
+function searchCep() {
+    var cepInsert = document.getElementById("Cep").value.replace("-", "");
+    $.getJSON(`https://viacep.com.br/ws/${cepInsert}/json/`, (itens) => {
+
+ 
+        var logradouro = itens.logradouro;
+        var bairro = itens.bairro;
+        var localidade = itens.localidade;
+        var uf = itens.uf;
+
+        console.log(logradouro);
+        console.log(bairro);
+        console.log(localidade);
+        console.log(uf);
+
+
+    });
+    
+};
