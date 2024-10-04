695a60170fba5bd66cf3d9379258183e01caec07
Xinlana1 <julianaoaguilar16@gmail.com>
2024-10-04 15:44:42 -0300

    close #5 - CEp inválido e número

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,55 @@
 $('#Cep').mask('00000-000');
-
+var number = document.querySelector("#number");
+number.disabled = true;
 
 
 
 
 function searchCep() {
     var cepInsert = document.getElementById("Cep").value.replace("-", "");
+
+
+    if (cepInsert.length != 8) {
+        number.disabled = true;
+        setCepInfo({});
+        errorAlert();
+        return;
+    }
+
+
+    clearErrorAlert();
     $.getJSON(`https://viacep.com.br/ws/${cepInsert}/json/`, (itens) => {
 
-        
-    setCepInfo(itens);
-       
+        if (itens.erro) {
 
-    });
-    
-};
+            
+
+         number.disabled = true;
+            setCepInfo({});
+            errorAlert();
+            return false;
+            
+        }
+        number.disabled = false;
+        setCepInfo(itens);
+        clearErrorAlert();
+    })
+
+}
+
+
+
+
+
+function errorAlert() {
+    document.getElementById("error").innerHTML = "<div class='text-danger'> CEP inválido! </div> ";
+}
+
+
+function clearErrorAlert() {
+    document.getElementById("error").innerHTML = "";
+}
+
 
 function setCepInfo(itens) {
     document.getElementById("address").value = itens.logradouro || " ";
@@ -22,3 +57,4 @@ function setCepInfo(itens) {
     document.getElementById("city").value = itens.localidade || " ";
     document.getElementById("state").value = itens.uf || " ";
 }
+
